85b4193f45ca788855661275d993afdc980a8ff3
Rayzen7 <nugrahasetio6@gmail.com>
2024-10-31 07:44:01 +0700

    update

--- src/components/sejarah/Galery.jsx
+++ src/components/sejarah/Galery.jsx
@@ -12,7 +12,7 @@ import { EffectCards } from 'swiper/modules';
 
 const Galery = () => {
   return (
-    <div className="lg:w-[600px] lg:px-0 px-9 w-full h-auto mx-auto block lg:my-32 my-28">
+    <div className="lg:w-[600px] lg:px-0 px-9 w-[320] overflow-hidden h-auto mx-auto block lg:my-32 my-28">
         <h1 className='text-center font-poppins font-[600] text-[32px] lg:text-[40px] mb-20'>Galery <span className='text-red-600'>Sumpah Pemuda</span></h1>
       <Swiper
         modules={[EffectCards]}
@@ -21,22 +21,22 @@ const Galery = () => {
         className='lg:mt-0 -mt-10'
       >
         <SwiperSlide>
-          <img src={img1} alt="Image 1" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img1} alt="Image 1" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img2} alt="Image 2" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img2} alt="Image 2" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img3} alt="Image 3" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img3} alt="Image 3" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img4} alt="Image 3" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img4} alt="Image 3" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img5} alt="Image 3" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img5} alt="Image 3" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img6} alt="Image 3" className="lg:w-[600px] w-full h-auto object-cover rounded-xl" />
+          <img src={img6} alt="Image 3" className="lg:w-[600px] w-[320] h-auto object-cover rounded-xl" />
         </SwiperSlide>
       </Swiper>
     </div>
